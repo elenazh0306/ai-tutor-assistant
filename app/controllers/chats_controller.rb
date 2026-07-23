ab865a8582a8ff587ad2b635254dbe5a8a78cdaa
Elena Zhukova <ellena.zhukof@gmail.com>
2026-07-23 14:13:17 +0300

    Setup ruby_llm gem

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,2 +1,17 @@
 class ChatsController < ApplicationController
+
+  def create
+    @subject = Subject.find(params[:subject_id])
+
+    @chat = Chat.new(title: "Untitled")
+    @chat.challenge = @challenge
+    @chat.user = current_user
+
+    if @chat.save
+      redirect_to chat_path(@chat)
+    else
+      @chats = @challenge.chats.where(user: current_user)
+      render "challenges/show"
+    end
+  end
 end
