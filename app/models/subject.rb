febf3911d9e390454a6805ce3dbfbb49db4e7b66
Thomas Davidsen <260642635+Thomas-H-D@users.noreply.github.com>
2026-07-25 13:31:58 +0900

    add this line to subject model - has_many :chats, dependent: :destroy

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,7 +2,7 @@ class Subject < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   belongs_to :user
   belongs_to :tutor
-  has_many :chats
+  has_many :chats, dependent: :destroy
   has_many :materials
   has_many :tests
 end
